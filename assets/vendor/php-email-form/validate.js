e21e2b4b53264a33754340f55c4ead0cfc33a48f
jenniferpayano <arlenepayano@gmail.com>
2020-05-25 16:34:38 -0400

    Contact form now works

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -3,13 +3,13 @@ jQuery(document).ready(function($) {
 
   //Contact
   $('form.php-email-form').submit(function() {
-   
+
     var f = $(this).find('.form-group'),
       ferror = false,
       emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
 
     f.children('input').each(function() { // run all inputs
-     
+
       var i = $(this); // current input
       var rule = i.attr('data-rule');
 
@@ -100,26 +100,27 @@ jQuery(document).ready(function($) {
       this_form.find('.error-message').slideDown().html('The form action property is not set!');
       return false;
     }
-    
-    this_form.find('.sent-message').slideUp();
-    this_form.find('.error-message').slideUp();
-    this_form.find('.loading').slideDown();
-    
-    $.ajax({
-      type: "POST",
-      url: action,
-      data: str,
-      success: function(msg) {
-        if (msg == 'OK') {
-          this_form.find('.loading').slideUp();
-          this_form.find('.sent-message').slideDown();
-          this_form.find("input:not(input[type=submit]), textarea").val('');
-        } else {
-          this_form.find('.loading').slideUp();
-          this_form.find('.error-message').slideDown().html(msg);
-        }
-      }
-    });
+
+    // this_form.find('.sent-message').slideUp();
+    // this_form.find('.error-message').slideUp();
+    // this_form.find('.loading').slideDown();
+    // $.ajax({
+    //      type: "POST",
+    //      url: action,
+    //      data: str,
+    //      success: function(msg) {
+    //        if (msg == 'OK') {
+    //          this_form.find('.loading').slideUp();
+    //          this_form.find('.sent-message').slideDown();
+    //          this_form.find("input:not(input[type=submit]), textarea").val('');
+    //          this_form.reset();
+    //        } else {
+    //          this_form.find('.loading').slideUp();
+    //          this_form.find('.error-message').slideDown().html(msg);
+    //        }
+    //      }
+    //    });
+
     return false;
   });
 
